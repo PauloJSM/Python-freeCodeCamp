d5f021c00a54b88c9c3230d2942b7ba9b53625a6
PauloJSM <hickbullionmount@gmail.com>
2024-01-02 23:02:08 +0000

    Added variablesDataTypes.py file

---
+++ variablesDataTypes.py
@@ -0,0 +1,11 @@
+character_name = "Tom"
+character_age = "50"
+is_male = True
+is_female = False
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + "years old. ")
+
+# comment
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")

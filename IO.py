ce9b69a7937c39d707867663eaa5baf7d892afe8
PauloJSM <hickbullionmount@gmail.com>
2024-01-03 14:04:30 +0000

    Added IO.py file

---
+++ IO.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+print("Hello " + name)
+print("Hello " + name + "! Nice to meet you!")
+
+age = input("How old are you?\n")
+print("Isn't " + age + " a bit old to learn Python?")

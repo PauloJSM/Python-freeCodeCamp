59fb80bdad41229f3db0e16a5ff188d9d9f4a824
PauloJSM <hickbullionmount@gmail.com>
2024-01-03 13:55:51 +0000

    Added numbers.py file

---
+++ numbers.py
@@ -0,0 +1,48 @@
+# mathematical functions library import
+from math import *
+
+print(2)
+print(2.0987)
+print(-2.0987)
+print(3 + 4.5)
+print(3 * 4.5)
+print(3 / 4.5)
+print(3 * 4.5 + 5)
+print(3 * (4.5 + 5))
+
+# modulus operation
+print(10 % 3)       # returns the remainder
+
+my_num = 5
+print(my_num)
+print(str(my_num))
+print("O valor é " + str(my_num) + ".")
+
+# absolute value
+my_num = -5
+print(abs(my_num))
+
+# power
+print(pow(my_num, 2))       # (-5)^2
+print(pow(3, 3))        # 3^2
+
+# max
+print(max(4, 6))
+print(max(max(4, 6), 9))
+print(max('A', 'a'))
+
+# min
+print(min(4, -3))
+
+# round
+print(round(3.2))
+print(round(3.7))
+
+# floor
+print(floor(3.7))
+
+# ceiling
+print(ceil(4.7))
+
+# square root
+print(sqrt(16))
